9bb778abf96eb26955b3eb827fcce89b56aec062
matsutake <matsutake.pg@gmail.com>
2020-06-06 23:37:13 +0900

    feat: add slowest result

--- benchmark/bench.js
+++ benchmark/bench.js
@@ -15,6 +15,7 @@ export default function bench(targets) {
     .on('cycle', (event) => console.log(String(event.target)))
     .on('complete', function () {
       console.log('Fastest is ' + this.filter('fastest').map('name'))
+      console.log('Slowest is ' + this.filter('slowest').map('name'))
     })
     .run({ async: true })
 }
