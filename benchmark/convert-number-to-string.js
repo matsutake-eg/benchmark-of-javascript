308f243453a44d6718747df05ed6bb080403a4a3
matsutake <matsutake.pg@gmail.com>
2020-06-07 15:44:56 +0900

    feat: add benchmark of convert number to string

---
+++ benchmark/convert-number-to-string.js
@@ -0,0 +1,33 @@
+import bench from './bench.js'
+
+const loopCount = 10 ** 5
+
+function sringFunc() {
+  let ans = ''
+  for (let i = 0; i < loopCount; i++) {
+    ans += String(i)
+  }
+  return ans.length >= 0
+}
+
+function toSringFunc() {
+  let ans = ''
+  for (let i = 0; i < loopCount; i++) {
+    ans += i.toString()
+  }
+  return ans.length >= 0
+}
+
+function plusFunc() {
+  let ans = ''
+  for (let i = 0; i < loopCount; i++) {
+    ans += '' + i
+  }
+  return ans.length >= 0
+}
+
+bench([
+  { name: 'toString', func: toSringFunc },
+  { name: 'String', func: sringFunc },
+  { name: 'plus', func: plusFunc },
+])
